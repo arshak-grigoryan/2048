2ad3f32951659d8c95c6312dfad2482953542e9d
Arshak Grigoryan <52595017+arshak-grigoryan@users.noreply.github.com>
2020-02-28 01:47:32 +0400

    Update keyboardEvent.js

--- js/keyboardEvent.js
+++ js/keyboardEvent.js
@@ -1,8 +1,8 @@
 import { isArrFull, isThereAnyStep } from './checkValues.js';
 import arrowRight from './steps.js/rightStep.js';
-import arrowLeft from './steps.js/LeftStep.js';
-import arrowUp from './steps.js/UpStep.js';
-import arrowDown from './steps.js/DownStep.js';
+// import arrowLeft from './steps.js/LeftStep.js';
+// import arrowUp from './steps.js/UpStep.js';
+// import arrowDown from './steps.js/DownStep.js';
 import { gameState } from './gameState.js';
 
 export default function arrow(event){
@@ -12,18 +12,18 @@ export default function arrow(event){
             setTimeout(()=>document.getElementById('game_over').style.display = 'flex',200)
         } else{
             switch(event.key){
-                case 'ArrowLeft': return arrowLeft()
-                case 'ArrowUp': return arrowUp()
-                case 'ArrowRight': return arrowRight()
-                case 'ArrowDown': return arrowDown()
+//                 case 'ArrowLeft': return arrowLeft()
+//                 case 'ArrowUp': return arrowUp()
+//                 case 'ArrowRight': return arrowRight()
+//                 case 'ArrowDown': return arrowDown()
             }
         }
     } else{
         switch(event.key){
-            case 'ArrowLeft': return arrowLeft()
-            case 'ArrowUp': return arrowUp()
-            case 'ArrowRight': return arrowRight()
-            case 'ArrowDown': return arrowDown()
+//             case 'ArrowLeft': return arrowLeft()
+//             case 'ArrowUp': return arrowUp()
+//             case 'ArrowRight': return arrowRight()
+//             case 'ArrowDown': return arrowDown()
         }
     }
     }        
