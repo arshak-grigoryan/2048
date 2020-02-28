9459eb962fb4647462eb59ff17f1690f822ee6e7
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-02-28 04:57:39 +0400

    ...

---
+++ js/steps/upStep.js
@@ -0,0 +1,91 @@
+import setTileStyle from '../setTileStyle.js';
+import { isThereCurrentArrowStep, isReach2048 } from '../checkValues.js';
+import { tile_live_values } from '../liveValues.js';
+import randome_tile from '../random.js';
+import { currentScore, bestScore } from '../score.js';
+
+function upStepNull(){
+    for(let j = 0; j < tile_live_values.length; j++){
+        for(let i = 1; i < tile_live_values.length; i++){ // i = 1 -> exclude 1st row
+            if(tile_live_values[i][j] !== null && tile_live_values[i-1][j] === null){
+                let tile_live = document.getElementsByClassName(`tile_live${i}${j}`)[0]
+                tile_live.classList.remove(`tile_live${i}${j}`)
+                tile_live.classList.add(`tile_live${i-1}${j}`)
+                tile_live_values[i-1][j] = tile_live_values[i][j]
+                tile_live_values[i][j] = null
+            } 
+        }
+    }
+}
+
+function upStepSum(){
+    for(let j = 0; j < tile_live_values.length; j++){
+        for(let i = 1; i < tile_live_values.length; i++){ // i = 1 -> exclude 1st row
+            
+            let currentValue = tile_live_values[i][j],
+                nextValue = tile_live_values[i-1][j]
+
+            if(currentValue !== null && nextValue !== null && currentValue === nextValue){
+                let current_tile_live = document.getElementsByClassName(`tile_live${i}${j}`)[0]
+                current_tile_live.classList.remove(`tile_live${i}${j}`)
+                current_tile_live.classList.add(`tile_live_${i-1}${j}`)
+                current_tile_live.style.zIndex = '10'
+
+                let next_tile_live = document.getElementsByClassName(`tile_live${i-1}${j}`)[0]
+                next_tile_live.style.zIndex = '20'
+
+                let value = tile_live_values[i-1][j] * 2
+                currentScore['value'] += value
+                document.getElementById('current-score-value').textContent = currentScore['value']
+                bestScore(currentScore['value'])
+                setTileStyle(next_tile_live, value)
+                tile_live_values[i][j] = null
+                tile_live_values[i-1][j] = value
+                upStepNull()
+
+                setTimeout(()=>{
+                    current_tile_live.remove()
+                    next_tile_live.style.zIndex = '10'
+                    next_tile_live.style.transform = 'scale(1.25)'
+                    setTimeout(()=>{
+                        next_tile_live.style.transform = 'scale(1)'
+                    },100)
+                },100)                    
+            } 
+        }
+    }    
+}
+
+/*  tile appearing is possible when available at least one step(neighbour value is equal to it or equal to null) 
+    or after any step which already have been completed */
+    
+export default function arrowUp(){
+    let firstTime = true
+    if(firstTime){
+        if(isThereCurrentArrowStep('up')){
+            firstTime = false
+            for(let i = 0; i < tile_live_values.length-1; i++){
+                upStepNull()
+            }
+
+            upStepSum()  
+            if(isReach2048()){
+                setTimeout(()=>document.getElementById('game_win').style.display = 'flex',200) 
+            }
+            setTimeout(()=>randome_tile(),200) 
+        }
+    } 
+    else{
+        for(let i = 0; i < tile_live_values.length-1; i++){
+            upStepNull()
+        }
+
+        upStepSum()  
+        if(isReach2048()){
+            setTimeout(()=>document.getElementById('game_win').style.display = 'flex',200) 
+        }
+        if(isThereCurrentArrowStep('up')){
+            setTimeout(()=>randome_tile(),200) 
+        }        
+    }
+}
